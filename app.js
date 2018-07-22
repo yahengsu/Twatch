4d07d37b56326079931a50085bd68606b5b4159c
YaHeng Su <yaheng.su1@gmail.com>
2018-07-22 17:39:56 -0600

    some functionalities

--- app.js
+++ app.js
@@ -11,10 +11,46 @@ var options = {
         username: "yasungBot",
         password: "oauth:pfoa7y2gdrq927a56k01imcwrb3z7f"
     },
-    channels: ["#yasung"]
+    channels: ["#imaqtpie"]
 };
 
 var client = new tmi.client(options);
+
+var intervalMessages = 0;
+var delta = 0;
+
+var previousAverage = 0;
+var currentAverage = 0;
+var maxAverage = 0;
+
+var spikeConstant = 2.3;
+
+var uniqueChatMessages = 0;
+var totalChatMessages = 0;
+
+var chatMessagesRaw = [];
+
+
+setInterval(getAverage, 8000);
+
+function getAverage() {
+    previousAverage = currentAverage;
+    currentAverage = intervalMessages/5;
+    if(currentAverage > maxAverage) {
+        maxAverage = currentAverage;
+    }
+    if((currentAverage/spikeConstant) > previousAverage) {
+        detectHotspot();
+    }
+    intervalMessages = 0;
+}
+var intervalLog = setInterval( () => {
+    console.log("prevAvg: " + previousAverage + " currAvg: " + currentAverage + " maxAvg: " + maxAverage);
+    console.log("totalMsgs: " + totalChatMessages + " uniqueMsgs: " + uniqueChatMessages);
+}, 8000);
+
+
+//trimmed mean = take middle 80% values and forget about top/bottom 10%
 
 // Connect the client to the server..
 client.connect();
@@ -24,6 +60,46 @@ client.on("chat", (channel, userstate, message, self) => {
     if (self) return;
     
     if(message === "!help") {
-        
+        client.action(channel, "I'm just a bot, sorry!");
     }
+    
+    if (chatMessagesRaw.indexOf(message) > -1) {
+        totalChatMessages++;
+    }
+    else {
+        uniqueChatMessages++;
+        totalChatMessages++;
+        chatMessagesRaw.push(message)
+    }
+    intervalMessages++;
 });
+
+client.on("join", (channel, username, self) => {
+    // Do your stuff.
+    console.log(username + "has joined the channel")
+});
+
+client.on("logon", () => {
+    // Do your stuff.
+});
+
+client.on("disconnected", (reason) => {
+    // Do your stuff.
+});
+
+
+function emoteGraph() {
+    //
+}
+
+function detectHotspot() {
+ /*
+    Determine real time chat hotspots
+    Surge in chat activity after good/bad play is made, can be useful for detecting highlights 
+ */
+    console.log("chat spike detected");
+}
+
+function isUniqueMessage() {
+
+}
