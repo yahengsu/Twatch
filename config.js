5f50b6dbbb71619a6bf1accff725ffc65a3c74c6
YaHeng Su <yaheng.su1@gmail.com>
2018-08-06 17:54:26 -0400

    stuff

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
+const username = "yasungBot";
+const password = "oauth:pfoa7y2gdrq927a56k01imcwrb3z7f";
 
 module.exports = {
-    username: "yasungBot",
-    password: "oauth:pfoa7y2gdrq927a56k01imcwrb3z7f"
+    username,
+    password
 };
