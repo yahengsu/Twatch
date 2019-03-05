d499179d702090f735ab880e600eed9f79202fc9
YaHeng Su <yaheng.su1@gmail.com>
2019-03-05 16:55:39 -0500

    overhaul

--- app.js
+++ app.js
@@ -1,486 +1,148 @@
-var tmi = require("tmi.js");
-var fs = require('fs');
-var constants = require('./constants.js');
-var requests = require("./requests.js");
-var plotly = require("plotly")(constants.plotlyUser, constants.plotlyKey);
-var emotes = JSON.parse(fs.readFileSync('global.json','utf8'));
-var bttvEmotes = JSON.parse(fs.readFileSync('bttvemotes.json', 'utf8'));
+const tmi = require("tmi.js");
+const vars = require("./variables");
+const requests = require("./requests");
+const client = new tmi.client(vars.tmi);
+const mongo = require('mongodb').MongoClient;
+const assert = require('assert');
 
-const express = require('express');
-const routes = require('./routes');
-const app = express();
-const port = process.env.PORT || 5000;
+const mongoURL = 'mongodb://localhost:27017';
+const dbName = 'commands';
 
-var appDate = new Date();
+mongo.connect(mongoURL, (err, client) => {
+    assert.equal(null, err);
+    console.log("Successfully connected to Database Server");
+    const db = client.db(dbName);
+    client.close();
+})
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+client.on("message", onMessage);
+client.on("connected", onConnect);
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Connected!' });
-  });
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    res.render('../views/App');
-});
-app.get('/graphs/0', (req, res) => {
-    res.send({
-        data: [
-            {  
-                x: timeValues,
-                y: avgChatMsgs,
-                type: 'scatter',
-                mode: 'line+points',
-                marker: {color: 'blue'}
-            }
-        ],
-        layout: {
-            title: 'Average Chat Messages per second',
-            xaxis: {title: 'Time (H:M)'},
-            yaxis: {title: 'Messages per second '}
-        }
-    });
-});
-
-app.get('/graphs/1', (req, res) => {
-    res.send({
-        data: [
-            {  
-                x: timeValues,
-                y: avgViewTime,
-                type: 'scatter',
-                mode: 'line+points',
-                marker: {color: 'blue'}
-            }
-        ],
-        layout: {
-            title: 'Average viewer view time',
-            xaxis: {title: 'Time (H:M)'},
-            yaxis: {title: 'Time (H:M)'}
-        }
-    });
-    
-});
-
-app.get('/graphs/2', (req, res) => {
-    res.send({
-        data: [
-            {  
-                x: timeValues,
-                y: chatSpikes,
-                type: 'scatter',
-                mode: 'line+points',
-                marker: {color: 'blue'}
-            }
-        ],
-        layout: {
-            title: 'Chat spikes (Chat msgs/second in interval > 2.5x avg chat msgs/second)',
-            xaxis: {title: 'Time (H:M)'},
-            yaxis: {title: 'Time (H:M)'}
-        }
-    });
-});
-
-app.get('/graphs/3', (req, res) => {
-    res.send({
-        data: [
-            {  
-                x: timeValues,
-                y: emoteUsage,
-                type: 'scatter',
-                mode: 'line+points',
-                marker: {color: 'blue'}
-            }
-        ],
-        layout: {
-            title: 'Emote Usage',
-            xaxis: {title: 'Time (H:M)'},
-            yaxis: {title: 'Time (H:M)'}
-        }
-    });
-});
-
-app.get('/graphs/4', (req, res) => {
-    res.send({
-        data: [
-            {  
-                x: timeValues,
-                y: uniqueness,
-                type: 'scatter',
-                mode: 'line+points',
-                marker: {color: 'blue'}
-            }
-        ],
-        layout: {
-            title: 'Chat uniqueness',
-            xaxis: {title: 'Time (H:M)'},
-            yaxis: {title: 'Time (H:M)'}
-        }
-    });
-});
-app.get('/graphs/5', (req, res) => {
-    res.send({
-        data: [
-            {  
-                x: timeValues,
-                y: totalChatMessages,
-                type: 'scatter',
-                mode: 'line+points',
-                marker: {color: 'blue'}
-            }
-        ],
-        layout: {
-            title: 'Total Chat messages',
-            xaxis: {title: 'Time (H:M)'},
-            yaxis: {title: 'Time (H:M)'}
-        }
-    });
-});
-
-let fileName = 'temp';
-let fileNameConst = 1;
-
-var options = {
-    options: {
-        debug: true
-    },
-    connection: {
-        reconnect: true
-    },
-    identity: {
-        username: constants.username,
-        password: constants.password
-    },
-    channels: constants.channels
-};
-
-var client = new tmi.client(options);
-
-var prevAvg = 0;
-var currAvg = 0;
-var maxAvg = 0;
-var avgViewerTime = 0;
-var channelViewers = {};
-var totalTime = 0;
-var intervals = 0;
-var numEmotes = 0;
-var intervalMessages = 0;
-let spikeConstant = 2.5;
-const viewerInterval = 10000;
-const debugInterval = 10000;
-
-//can do chat uniqueness -> percent of unique msgs compared total mgss
-var uniqueChatMessages = 0;
-var totalChatMessages = 0;
-var timeValues = []; // x axis
-var uniqueness = []; //  4
-var avgChatMsgs = []; // 0
-var avgViewTime = []; // 1
-var totalChatMsgs = []; // 5
-var emoteUsage = []; // 3
-var chatSpikes = [] // 2
-var newUsers = [];
-
-function updateData() {
-    avgChatMsgs.push(currAvg);
-    var unique = uniqueChatMessages / totalChatMessages;
-    uniqueness.push(unique);
-    avgViewTime.push(avgViewerTime);
-    totalChatMsgs.push(totalChatMessages);
-    emoteUsage.push(numEmotes);
+client.connect();
+function onConnect(add, port) {
+    console.log(`connected to ${add}:${port}`);
 }
 
-var chatMsgs = [];
-var chatMsgsPerInterval = [];
-//return top 10 emotes in chat
-var emoteMsgs = {};
-//for copypastas want to do some detection, maybe characters in message has to be at least 12 characters to filter out emotes and low quality spam
-var copypastas = {};
+const COMMAND_INTERVAL = 900000; // - 15 minute interval
+// const COMMAND_INTERVAL = 1000;
+setInterval(twitchPrimeReminder, COMMAND_INTERVAL);
+setInterval(waterReminder, COMMAND_INTERVAL * 2);
 
-var dataInterval = 5000;
-setInterval(getAverage, debugInterval);
-setInterval(debugLog, debugInterval);
-setInterval(averageViewerTime, viewerInterval);
-setInterval(updateData, dataInterval);
-setInterval(updateTimeData, dataInterval);
-
-
-function updateTimeData() {
-    var currDate = new Date();
-    var difference = (appDate.getTime() - currDate.getTime()) / 1000;
-    var hours = difference /60/60;
-    var hoursFloored = Math.floor(hours);
-    var minutes = Math.round((hours - hoursFloored) * 60);
-    var value = hoursFloored + ":" + minutes;
-    timeValues.push(value);
-
-}
-function getAverage() {
-    prevAvg = currAvg;
-    currAvg = intervalMessages/5;
-    if(currAvg > maxAvg) {
-        maxAvg = currAvg;
+//METHOD IS RUN EVERY TIME A NEW MESSAGE IS TYPED IN CHAT
+function onMessage(chan, userstate, message, self) {
+    var channel = chan.substring(1);
+    if (self) {
+        return;
     }
-    if((currAvg/spikeConstant) > prevAvg) {
-        chatSpikes.push(currAvg);
+    const msg = message.trim();
+    if (msg === "#title") {
+        titleHandler(channel, userstate);
     }
-    intervalMessages = 0;
-}
-
-
-function debugLog() {
-    console.log("prevAvg: " + prevAvg + " currAvg: " + currAvg + " maxAvg: " + maxAvg);
-    console.log("totalMsgs: " + totalChatMessages + " uniqueMsgs: " + uniqueChatMessages);
-   // console.log(`emoteMsgs: ${JSON.stringify(emoteMsgs)}`);
-   // console.log(`copypastas: ${JSON.stringify(copypastas)}`)
-}
-/* 
-    question of what we want to plot
-        - uniqueness of chat over time -> uniqueMsgs/totalMsgs * 100
-        - average chat msgs over time -> totalMsgs / 100
-        - average viewer time over time 
-        - average emote usage over time 
-        - total chat msgs over time 
-
-    other non plotable things that should be included on dashboard
-        - top copypastas (maybe top 5) + number of times spammed
-        - top emotes + number of times used
-        - twitch chat embedded on the side
-        - max avg chat activity
-
-
-*/
-
-
-// Connect the client to the server..
-client.connect().then(function(data) {
-    console.log(`CONNECTED: ${data}`);
-}).catch(function(err) {
-    console.log(err);
-});
-
-client.on("chat", onChatHandler);
-client.on("join", onJoinHandler);
-
-
-
-function onChatHandler(channel, userstate, message,self) {
-    if (self) return;
-    //storage of chat msgs for chat analysis
-    chatMsgsPerInterval.push(message);
-    chatMsgs.forEach((element) => {
-        if (element.message == message) {
-            element.count++;
-        }
-    });
-    if(chatMsgs.includes(message)) {
-        chatMsgs[message] += 1;
+    if (msg === "#followage") {
+        followageHandler(channel, userstate);
     }
-    else {
-       chatMsgs[message] = 1;
-       uniqueChatMessages++;
-    }
-    intervalMessages++;
-
-    //copypasta checker and data entry
-    if(isCopypasta(message)) {
-        if(copypastas.hasOwnProperty(message)){
-            copypastas[message] += 1;
-        }
-        else {
-            copypastas[message] = 1;
-        }
-    }
-    //emote checker and data entry
-    Object.keys(emotes).forEach((key) => {
-        if(message.includes(key)) {
-            if(emoteMsgs.hasOwnProperty(key)) {
-                emoteMsgs[key] += 1;
-            }
-            else {
-                emoteMsgs[key] = 1;
-            }
-            numEmotes++;
-        }
-    });
-    Object.keys(bttvEmotes).forEach((key) => {
-        if(message.includes(key)) {
-            if(emoteMsgs.hasOwnProperty(key)) {
-                emoteMsgs[key] += 1;
-            }
-            else {
-                emoteMsgs[key] = 1;
-            }
-            numEmotes++;
-        }
-    })
-
-    //commands
-    if(message === "#followage") {
-        followageDateHandler(channel, userstate);
-    }
-    if(message === "#uptime") {
+    if (msg === "#uptime") {
         uptimeHandler(channel, userstate);
     }
-    if(message === "#title") {
-        streamTitleHandler(channel, userstate);
+    if (msg === "#clear") {
+        clearChatHandler(channel);
     }
-    if(message === "#blackfr0st") {
-        client.say(channel, "https://imgur.com/0I3W6fQ");
+    if (msg === "#emotes") {
+        emoteModeHandler(channel);
     }
-    if(message === "#copypasta") {
-        //copypastasHandler()
+    if (msg === "#help") {
+        const message = `gist.github.com`;
+        client.say(channel, message);
     }
-    if(message === "#emotes") {
-        //topEmotesHandler()
+    if (msg === "#prime") {
+        twitchPrimeReminder(channel);
     }
-    if(message === "#opgg") {
-        //opggHander()
+    if(msg.startsWith("#rank")) {
+        getRankHandler(channel, userstate, msg);
     }
-    if(message === "#viewer") {
-        averageViewerTime(channel, userstate);
+    if (msg.startsWith("#set")) {
+        setCommandHandler(channel);
     }
-    if(message === "#viewers") {
-        //viewersHandler();
+    if(msg.startsWith("#opgg")) {
+        getOPGGHandler(channel, userstate, msg);
     }
-    
-} 
-
-function followageDateHandler(channel, userstate) {
-    var prefix = "@" + userstate.username + ", ";
-    requests.followage(channel, userstate, (res) => {
-        if(res.total < 1) {
-            client.say(channel, prefix + userstate.username + " is not following " + channel.substring(1) + "!");
-        }
-        else if (res.total == 1) {
-            var followDate = new Date(res.data[0].followed_at);
-            var currentDate = new Date();
-            var difference = (currentDate.getTime() - followDate.getTime()) / 1000; //difference in date in seconds
-            var days = difference/60/60/24;
-            var daysFloored = Math.floor(days);
-            var hours = Math.round((days - daysFloored) * 24);
-            client.say(channel, prefix + userstate.username + " has been following " + channel.substring(1) + " for " + daysFloored + " days and " + hours + " hours!");
-        }
-    });
-}
-
-function uptimeHandler(channel, userstate) {
-    var prefix = "@" + userstate.username + ", ";
-    requests.uptime(channel, (res) => {
-        if(res.data === undefined || res.data.length == 0) {
-            client.say(channel, prefix + channel.substring(1) + " is not live!");
-        }
-        else if(res.data[0].type === "live") {
-            var startTime = new Date(res.data[0].started_at);
-            var currentDate = new Date();
-            var difference = (currentDate.getTime() - startTime.getTime()) / 1000;
-            var hours = difference /60/60;
-            var hoursFloored = Math.floor(hours);
-            var minutes = Math.round((hours - hoursFloored) * 60);
-            client.say(channel, prefix + channel.substring(1) + " has been streaming for " + hoursFloored + " hours and " + minutes + " minutes!");
-        }
-    });
-}
-
-function streamTitleHandler(channel, userstate) {
-    var prefix = "@" + userstate.username + ", ";
-    requests.streamTitle(channel, (res) => {
-        if(res.data === undefined || res.data.length == 0) {
-            client.say(channel, prefix + channel.substring(1) + " is not live!");
-        }
-        else if(res.data[0].type === "live") {
-            var title = res.data[0].title;
-            client.say(channel, prefix + "Current Stream Title: " + title);
-        }
-    });
-}
-
-function topEmotesHandler() {
-    //sort emotes
-    //return top 10
-    
-}
-function copypastasHandler(channel, message) {
-    //sort copypastas
-    //return top 10
-}
-
-client.on("disconnected", (reason) => {
-    console.log(`Disconnected: ${reason}`);
-});
-client.on("connected", function (address, port) {
-    console.log("connected");
-});
-
-
-function onJoinHandler(channel, username, self) {
-    console.log(`${username} has joined the channel`);
-    newUsers.push(username)
 }
 
 
-function emoteGraph() {
+/* SCHEDULED CHAT MESSAGES */
+
+//TWITCH PRIME REMINDER !prime
+
+function twitchPrimeReminder(channel) {
+    const msg = `Did you know you get a free subscription to your favourite streamer every 30 days with your Amazon Prime membership? Learn more here https://twitch.tv/prime`;
+    client.say(channel, msg);
+}
+
+async function waterReminder(channel) {
+    const obj = await requests.waterHandler(channel);
+    if(obj !== -1) {
+        const waterML = (obj.hours * 250) + (obj.minutes * 4); // water amount in mL
+        const waterOZ = (waterML / 29.574).toFixed(2);
+        const msg = `@${channel}, you have been streaming for ${obj.hours} hours and ${obj.minutes} minutes. By now you should have consumed ${waterML} mL / ${waterOZ} oz. of water to for optimal hydration.`;
+        client.say(channel, msg);
+        console.log(msg);
+    }
     
 }
 
-function handleHotspot() {
-    //plot to graph
-    console.log("chat spike detected");
+/* TWITCH API REQUESTS */
+
+//STREAM TITLE COMMAND !title
+
+async function titleHandler(channel, userstate) {
+    const msg = await requests.getTitle(channel, userstate);
+    client.say(channel,msg);
 }
 
 
-/*https://tmi.twitch.tv/group/user/USERNAME/chatters
-    For getting average viewer looktime 
-    if user is in object, add minute to their time every minute 
-    if user is not in object, then add them to object with initial value of 0 
-    only looks at viewers and not moderators, bots, twitch admin, etc
-    if object,hasOwnProperty(username)
-        object.username += 0.1
-        total viewer watch time / viewers
-        plot graph over time 
-*/
+//FOLLOWAGE COMMAND !followage
+async function followageHandler(channel, userstate) {
+    const msg = await requests.getFollowage(channel, userstate);
+    client.say(channel,msg);
+}
 
-function averageViewerTime() {
-    var channel  = "#imaqtpie";
-        requests.getViewerList(channel, (res) => {
-            var viewerList = res.chatters.viewers; //get viewerlist
-            viewerList.forEach((viewer) => {
-                if(channelViewers.hasOwnProperty(viewer)) {
-                    channelViewers[viewer] += (viewerInterval/1000); //add seconds 
-                }
-                else {
-                    channelViewers[viewer] = 0; 
-                }
-            });
-            let totalViewers = Object.keys(channelViewers).length;
-            Object.keys(channelViewers).forEach((key) => {
-                totalTime += Number(channelViewers[key]);
-            });
-            if(totalTime !== 0) {
-                avgViewerTime = Number(totalTime/totalViewers);
-            }
-            totalTime = 0;
-            intervals++;
-            console.log(`Average Viewer Time (s) : ${avgViewerTime}`);
-            console.log(`Max Viewer Time (s) ${intervals * viewerInterval/1000}`);
-        });
-    // });
+//UPTIME COMMAND !uptime
+async function uptimeHandler(channel, userstate) {
+   const msg = await requests.getUptime(channel, userstate);
+   client.say(channel, msg);
 }
 
 
-function isCopypasta(message) {
-    if(message.length >= 12) {
-        if(chatMsgsPerInterval.includes(message)){
-            return true;
-        }
-    }
-    else {
-        return false;
-    }
+//CLEAR CHAT COMMAND !clear
+
+function clearChatHandler(channel) {
+    client.clear(channel)
+    .then(() => {
+        console.log("CHAT CLEARED");
+    }).catch((err) => {
+        console.log(err);
+    }); 
 }
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function(err){
-        if (err) console.log(err);
-        console.log("Successfully Written to File.");
-    });
+
+
+
+/* LEAGUE OF LEGENDS API REQUESTS */
+
+
+async function getRankHandler(channel, userstate, msg) {
+    const summonerName = msg.split(" ")[1];
+    const reply = await requests.getSummonerRank(summonerName);
+    client.say(channel,`@${userstate.username}, ${reply}`);
 }
+
+
+function getOPGGHandler(channel, userstate, msg) {
+    const username = msg.split(" ")[1];
+    const region = opts.region;
+    const link = `@${userstate.username}, https://${region}.op.gg/summoner/userName=${username}`
+    client.say(channel, link);
+}
+
+
+
+/* */
--- requests.js
+++ requests.js
@@ -1,106 +1,178 @@
-var rp = require("request-promise-native");
-var constants = require("./constants.js");
+const axios = require('axios');
+const vars = require("./variables");
+
+
+/* TWITCH API REQUESTS */
+
+async function getTitle(channel, userstate) {
+    const url = `https://api.twitch.tv/helix/streams/?user_login=${channel}`;
+    const headers = {
+        "Client-ID": vars.clientID
+    };
+    try {
+        const response = await axios.get(url, {headers});
+        const res = await response.data;
+
+        if(res.data === undefined || res.data.length == 0) {
+            const msg = `@${userstate.username}, ${channel} is currently offline!`;
+            return msg;
+        }
+        else if(res.data !== undefined && res.data.length != 0) {
+            const msg = `@${userstate.username}, ${res.data[0].title}`;
+            return msg;
+        }
+    }
+    catch(e) {
+        commonCatch(e);
+    }
+
+}
+
+async function getFollowage(channel, userstate) {
+    const url = `https://2g.be/twitch/following.php?user=${userstate.username}&channel=${channel}&format=ymwd`;
+    try {
+        const response = await axios.get(url);
+        const res = await response.data;
+        console.log(data);
+    
+        const msg = `@${res}!`;
+        return msg;
+    }
+    catch(e) {
+        commonCatch(e);
+    }
+}
+
+async function getUptime(channel, userstate) {
+    const url = `https://api.twitch.tv/helix/streams?user_login=${channel}`;
+    const headers = {
+        "Client-ID": vars.clientID
+    };
+    try {
+        const response = await axios.get(url, {headers});
+        const res = await response.data;
+        if(res.data === undefined || res.data.length == 0) {
+            var notLiveMsg = `@${userstate.username}, ${channel} is currently offline!`;
+            return notLiveMsg;
+        }
+        else if(res.data !== undefined && res.data[0].type === "live") {
+            var startTime = new Date(res.data[0].started_at);
+            var currentDate = new Date();
+            var difference = (currentDate.getTime() - startTime.getTime()) / 1000;
+            var hours = difference/3600;
+            var hoursFloored = Math.floor(hours);
+            var minutes = Math.round((hours - hoursFloored) * 60);
+            var msg = `@${userstate.username}, ${channel} has been streaming for ${hoursFloored} hours and ${minutes} minutes!`
+            return msg;
+        }
+    }
+    catch(e) {
+        commonCatch(e);
+    }
+   
+}
+
+async function waterHandler(channel) {
+    const url = `https://api.twitch.tv/helix/streams?user_login=${channel}`;
+    const headers = {
+        "Client-ID": vars.clientID
+    };
+    
+    try {
+        const response = await axios.get(url, {headers});
+        const res = await response.data;
+        if(res.data === undefined || res.data.length == 0) {
+            return -1;
+        }
+        else if(res.data !== undefined && res.data[0].type === "live") {
+            var startTime = new Date(res.data[0].started_at);
+            var currentTime = new Date();
+            var diff = (currentTime.getTime() - startTime.getTime()) / 1000;
+            var hours = diff/3600;
+            var minutes = Math.round((hours - Math.floor(hours)) * 60);
+            const obj = {
+                hours: Math.floor(hours),
+                minutes: minutes
+            }
+            return obj;
+        }
+    }
+    catch(e) {
+        commonCatch(e);
+    }
+}
+
+
+/* RIOT GAMES API REQUESTS */
+
+async function getRiotID(summonerName) {
+
+    const headers = {
+        "X-Riot-Token": vars.riotToken
+    };
+    const url = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`;
+    try {
+        const response = await axios.get(url, {headers});
+        const res = await response.data;
+        const id = res.id;
+
+        return id;
+    }
+    catch(e) {
+        commonCatch(e);
+    }
+}
+
+async function getSummonerRank(summonerName) {
+    const summonerID = await getRiotID(summonerName);
+    const headers = {
+        "X-Riot-Token": vars.riotToken
+    }
+    const url = `https://${vars.region}1.api.riotgames.com/lol/league/v4/positions/by-summoner/${summonerID}`;
+    try {
+        const response = await axios.get(url, {headers});
+        const res = await response.data;
+        const msg = `${res[0].summonerName} current rank: ${res[0].tier} ${res[0].rank} ${res[0].leaguePoints}LP`;
+        return msg;
+
+    } catch (e) {
+        commonCatch(e);
+    }
+}
+
+
+
+
+/* MISC FUNCTIONS */
+
+function commonCatch(e) {
+    console.log(e.config);
+    if(e.response) {
+        console.log("RESPONSE");
+        console.log(e.response.data);
+        console.log(e.response.status);
+        console.log(e.response.headers);
+        return "-1";
+    }
+    else if(e.request) {
+        console.log("REQUEST");
+        console.log(e.request);
+        return "-2";
+    }
+    else {
+        console.log("ERROR", e.message);
+        return "-3";
+    }
+}
+
+
+
+
 
 module.exports = {
-    followage: followage,
-    getIDByUsername: getIDByUsername,
-    uptime: uptime,
-    streamTitle: streamTitle,
-    getViewerList: getViewerList
+    getTitle: getTitle,
+    getFollowage: getFollowage, 
+    getUptime: getUptime,
+    getSummonerRank: getSummonerRank,
+    waterHandler: waterHandler
 };
-
-function followage(channel, user, resp) {
-    var channelName = channel.substring(1);
-    getIDByUsername(channelName, (res) => {
-        var channelID = res.data[0].id;
-        var fromID = user["user-id"];
-        var baseURL = "https://api.twitch.tv/helix/users/follows";
-        var param = "?from_id=" + fromID + "&to_id=" + channelID;
-        var totalURL = baseURL + param;
-        var options = {
-            uri: totalURL,
-            headers: {
-                "Client-ID" : constants.ClientID,
-                "from_id" : fromID,
-                "to_id" : channelID
-            },
-            json: true
-        };
-        rp(options)
-            .then(resp)
-            .catch((err) => {
-                console.log(err);
-            })
-        });
-    
-
-}
-
-function uptime(channel, resp) {
-    var channelName = channel.substring(1);
-    var baseURL = "https://api.twitch.tv/helix/streams";
-    var param = "?user_login=" + channelName;
-    var totalURL = baseURL + param;
-    var options = {
-        uri: totalURL,
-        headers: {
-            "Client-ID" : constants.ClientID
-        },
-        json: true
-    };
-    rp(options)
-        .then(resp)
-        .catch((err) => {
-            console.log(err);
-        });
-}
-
-function streamTitle(channel, resp) {
-    var channelName = channel.substring(1);
-    var baseURL = "https://api.twitch.tv/helix/streams";
-    var param = "?user_login=" + channelName;
-    var totalURL = baseURL + param;
-    var options = {
-        uri: totalURL,
-        headers: {
-            "Client-ID" : constants.ClientID
-        },
-        json: true
-    };
-    rp(options)
-        .then(resp)
-        .catch((err) => {
-            console.log(err);
-        });
-}
-function getIDByUsername(username, res) {
-    var baseURL = "https://api.twitch.tv/helix/users";
-    var param = "?login=" + username;
-    var totalURL = baseURL + param;
-    var options = {
-        uri: totalURL,
-        headers: {
-            "Client-ID" : constants.ClientID
-        },
-        json: true
-    };
-    rp(options)
-        .then(res)
-        .catch((err) => {
-            console.log(err);
-        });
-}
-
-function getViewerList(channel, res) {
-    var channelName = channel.substring(1);
-    var url = `https://tmi.twitch.tv/group/user/${channelName}/chatters`;
-    var options = {
-        uri: url,
-        json: true
-    }
-    rp(options)
-        .then(res)
-        .catch((err) => {
-            console.log(err);
-        });
-}
